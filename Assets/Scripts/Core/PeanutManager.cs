749bd77d2b1c3882636ee967e5391ed0615ee84a
Cooper Martin <coopatroopa537@gmail.com>
2026-06-03 13:58:35 -0400

    Add debt system - players can borrow when low on peanuts

--- Assets/Scripts/Core/PeanutManager.cs
+++ Assets/Scripts/Core/PeanutManager.cs
@@ -6,9 +6,13 @@ public class PeanutManager : MonoBehaviour
 
     [SerializeField] private int startingPeanuts = 100;
     private int currentPeanuts;
+    private int debt = 0;
 
     public delegate void PeanutChangedDelegate(int newAmount);
     public event PeanutChangedDelegate OnPeanutChanged;
+
+    public delegate void DebtChangedDelegate(int debtAmount);
+    public event DebtChangedDelegate OnDebtChanged;
 
     private void Awake()
     {
@@ -21,11 +25,17 @@ public class PeanutManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
         currentPeanuts = startingPeanuts;
+        debt = 0;
     }
 
     public int GetPeanuts()
     {
         return currentPeanuts;
+    }
+
+    public int GetDebt()
+    {
+        return debt;
     }
 
     public bool SpendPeanuts(int amount)
@@ -42,7 +52,32 @@ public class PeanutManager : MonoBehaviour
     public void AddPeanuts(int amount)
     {
         currentPeanuts += amount;
+        
+        // Pay off debt first
+        if (debt > 0)
+        {
+            int paymentAmount = Mathf.Min(currentPeanuts, debt);
+            debt -= paymentAmount;
+            currentPeanuts -= paymentAmount;
+            OnDebtChanged?.Invoke(debt);
+        }
+        
         OnPeanutChanged?.Invoke(currentPeanuts);
+    }
+
+    public bool BorrowPeanuts(int amount)
+    {
+        // Can only borrow if peanuts <= 5
+        if (currentPeanuts <= 5)
+        {
+            currentPeanuts += amount;
+            debt += amount;
+            OnPeanutChanged?.Invoke(currentPeanuts);
+            OnDebtChanged?.Invoke(debt);
+            Debug.Log($"Borrowed {amount} peanuts. Total debt: {debt}");
+            return true;
+        }
+        return false;
     }
 
     public void SetPeanuts(int amount)
